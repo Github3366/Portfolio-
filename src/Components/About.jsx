b46898316cb2e77b69cf8c1cc773220156391e72
Github3366 <90845928+Github3366@users.noreply.github.com>
2024-06-01 15:26:24 +0000

    change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a dedicated and enthusiastic Computer Science student at univercity college of engg &tech,MGU. I am intrested in web development. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML",
+  "CSS",
+  "JAVA SCRIPT",
+  "java",
+  "MY SQL",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate for developing innovative software solutions and a keen interest in the fields of artificial intelligence and cybersecurity. With a solid foundation in programming languages such as Python, Java, and C++, I thrive on tackling complex problems and continuously expanding my technical skill set.";
 
 const About = () => {
   return (
